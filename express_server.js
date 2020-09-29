3eb1fa5ad59df51ee7506cc51ad4b6bad9e44df3
Graeme Chalmers <chalmers.graeme@gmail.com>
2020-09-29 01:31:38 +0000

    added a new url page

--- express_server.js
+++ express_server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+function generateRandomString() {
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 app.get("/", (req, res) => {
   res.send("Hello");
@@ -26,12 +32,23 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
+
+};
